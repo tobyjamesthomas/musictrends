6364836c8e67ca40b1bcc78fe41fd51784e6af97
Toby <toby.james.thomas@gmail.com>
2020-02-02 17:23:28 -0500

    Plot genre trends

--- app.py
+++ app.py
@@ -6,32 +6,32 @@ import pandas as pd
 
 df = pd.read_csv('data/data.csv')
 
+df = df.groupby(['genre', 'year'], as_index = False).mean()
+colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", '#000000']
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Graph(
-        id='life-exp-vs-gdp',
+        id='music-trends',
         figure={
             'data': [
                 dict(
-                    x=df[df['year'] == year]['year'],
-                    y=df[df['year'] == year]['num_words'],
-                    text=df[df['year'] == year]['title'],
-                    mode='markers',
+                    x=df[df['genre'] == genre]['year'],
+                    y=df[df['genre'] == genre]['num_words'],
                     opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=year
-                ) for year in df.year.unique()
+                    mode='lines',
+                    marker=dict(color = colors[i]),
+                    name=genre
+                ) for i, genre in enumerate(df.genre.unique())
             ],
             'layout': dict(
                 xaxis={'title': 'Year'},
                 yaxis={'title': 'Number of Words'},
                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                line={'shape': 'spline', 'smoothing': 10},
                 legend={'x': 0, 'y': 1},
                 hovermode='closest'
             )
--- data/aggregate.py
+++ data/aggregate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_json('data.json')
+df = pd.read_json('raw/data.json')
 
 output = pd.DataFrame()
 
@@ -12,6 +12,7 @@ for i in range(0, len(df)):
     neu = [None] * size
     pos = [None] * size
     compound = [None] * size
+    genre = [None] * size
 
     for j in range(0, size):
         neg[j] = decade.iloc[j].sentiment['neg']
@@ -19,10 +20,14 @@ for i in range(0, len(df)):
         pos[j] = decade.iloc[j].sentiment['pos']
         compound[j] = decade.iloc[j].sentiment['compound']
 
+        tags = decade.iloc[j].tags
+        genre[j] = tags[0] if tags else 'none'
+
     decade['sentiment_neg'] = neg
     decade['sentiment_neu'] = neu
     decade['sentiment_pos'] = pos
     decade['sentiment_compound'] = compound
+    decade['genre'] = genre
 
     path = r'year/' + str(df.iloc[i]['year']) + r'.csv'
     decade.to_csv(path)
